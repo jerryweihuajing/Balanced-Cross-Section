76e125a9dabde038f7189af22c603fa204a24965
jerryweihuajing <jerryweihuajing@126.com>
2019-08-14 14:38:35 +0800

    Update o_triangle.py

--- Object/o_triangle.py
+++ Object/o_triangle.py
@@ -2,106 +2,80 @@
 """
 Created on Sun Nov 18 21:45:23 2018
 
-@author:Wei Huajing
-@company:Nanjing University
-@e-mail:jerryweihuajing@126.com
+@author: Wei Huajing
+@company: Ameng Science and Technology Education Co., Ltd
+@e-mail: jerryweihuajing@126.com
 
-@title：基于位移量守恒的平衡恢复对象库-triangle
+@title：Object-triangle
 """
 
 import numpy as np
 
+import Geometry as Geom
+
 #==============================================================================  
-#定义一个三角形的类
+#define a trinagle class
 #==============================================================================  
 class triangle:
-    def __init__(self,ABC,
+    def __init__(self,
+                 ABC=None,
                  area=None):
-        
-        #ABC表示三角形三个顶点,初始化时就应当定义它
+
         self.ABC=[]
-        
-        #数组化
+
+        #transfrom them into np.array
         for pos in ABC:
+            
             self.ABC.append(np.array(pos))
  
-        #三个顶点的坐标
+        #three
         pos_A,pos_B,pos_C=self.ABC
         
-        #计算三条边长
-        AB=whj.Distance(pos_A,pos_B)
-        AC=whj.Distance(pos_A,pos_C)
-        CB=whj.Distance(pos_C,pos_B)
+        #Calculate three boundary length
+        AB=Geom.Distance(pos_A,pos_B)
+        AC=Geom.Distance(pos_A,pos_C)
+        CB=Geom.Distance(pos_C,pos_B)
         
+        #Helen formula
         a,b,c=CB,AC,AB
         p=(a+b+c)/2
 
-        #area表示三角形面积
+        #area: traingle area
         self.area=np.sqrt(p*(p-a)*(p-b)*(p-c))
     
-    #判断点是否在三角形内部   
+    #judge whether pos_P is inside the triangle  
     def IncludePoint(self,pos_P):
         
-        #还原ABC坐标
         pos_A,pos_B,pos_C=self.ABC
         
-        #向量化
+        #Vetorization
         pos_A=np.array(pos_A)
         pos_B=np.array(pos_B)
         pos_C=np.array(pos_C)
         
         pos_P=np.array(pos_P)
         
-        #使用方法2
-        method='2'
+        #vector method
+        _AP=pos_A-pos_P
+        _AC=pos_A-pos_C
+        _AB=pos_A-pos_B
+
+        #Calculate quadratic system of one variable
+        import sympy
         
-        #方法一：面积法
-        if method=='1':
-    
-            Area_PAB=triangle(pos_A,pos_B,pos_P).area  
-            Area_PAC=triangle(pos_A,pos_P,pos_C).area    
-            Area_PBC=triangle(pos_P,pos_B,pos_C).area 
-            
-            Area_ABC=triangle(pos_A,pos_B,pos_C).area
-            
-            Area_sum=Area_PAB+Area_PAC+Area_PBC
-            
-            #判断PAB,PAC,PBC的总面积和ABC是否相等   
-            if Area_sum==Area_ABC:
-                
-                return True  
-            else:
-                return False
+        u=sympy.Symbol('u')
+        v=sympy.Symbol('v')
         
-        #方法二：向量法      
-        if method=='2':
+        #The solution is an np.array
+        answer=sympy.solve([u*_AC[0]+v*_AB[0]-_AP[0],u*_AC[1]+v*_AB[1]-_AP[1]],[u,v])
+        
+        u,v=answer[u],answer[v]
+        
+        #Judgement conditions：0<=u<=1,0<=v<=1,0<=u+v<=1
+        if 0<=u<=1 and 0<=v<=1 and 0<=u+v<=1:
             
-            #向量法：_AP=u*_AC+v*_AB，其中_AP,_AB,_AC都是向量
-            _AP=pos_A-pos_P
+            return True  
+        
+        else:
             
-            _AC=pos_A-pos_C
-            _AB=pos_A-pos_B
-            
-            #解方程组
-            #_AP[0]=u*_AC[0]+v*_AB[0]
-            #_AP[1]=u*_AC[1]+v*_AB[1]
-           
-            #计算二元一次方程组
-            import sympy
-            
-            u=sympy.Symbol('u')
-            v=sympy.Symbol('v')
-            
-            #得到的解是一个数组
-            answer=sympy.solve([u*_AC[0]+v*_AB[0]-_AP[0],u*_AC[1]+v*_AB[1]-_AP[1]],[u,v])
-            
-#            print(answer[u],answer[v])
-            
-            u,v=answer[u],answer[v]
-            
-            #判断条件：0<=u<=1,0<=v<=1,0<=u+v<=1
-            if 0<=u<=1 and 0<=v<=1 and 0<=u+v<=1:
-                
-                return True  
-            else:
-                return False   
+            return False   
